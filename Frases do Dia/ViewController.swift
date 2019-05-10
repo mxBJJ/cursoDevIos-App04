cb73b17ce59b103568d874af53b665daa15259c5
Max Mendes <maxmendes0509@gmail.com>
2019-05-10 12:53:28 -0300

    Initial Commit

---
+++ Frases do Dia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Frases do Dia
+//
+//  Created by Max Mendes on 10/05/19.
+//  Copyright © 2019 Curso iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
